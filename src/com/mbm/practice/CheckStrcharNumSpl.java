cfa31d8ec0917028d33765d1023428ee95f2ec28
Bala Murali <balamuralim111@gmail.com>
2023-02-05 10:25:05 +0530

    commiting CheckStrcharNumSpl file into branchDev

---
+++ src/com/mbm/practice/CheckStrcharNumSpl.java
@@ -0,0 +1,28 @@
+package com.mbm.practice;
+
+import java.util.Scanner;
+
+public class CheckStrcharNumSpl {
+	public static void main(String[] args) {
+
+		System.out.println("Enter a String");
+		Scanner scanner = new Scanner(System.in);
+
+		String str = scanner.nextLine();
+		
+		System.out.println(str);
+		
+
+			if(str.matches("^[a-zA-Z0-9!#$%&(*+),-/@]*$")) {
+				System.out.println(str);
+				System.out.println("valid password");
+				System.out.println(str);
+			}else {
+				System.out.println("Invalid Password");
+			}
+			
+		
+		scanner.close();
+	}
+
+}
